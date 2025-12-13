a97b08da44ce0787cbcc7e03251cb7bf393ab166
Franz Ivan De Villa <franzivan.devilla@gmail.com>
2025-12-13 14:30:05 +0800

    Try changing route logic

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,6 @@ const withMDX = mdx({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
-  serverExternalPackages: ["yt-search", "cheerio"],
   transpilePackages: ["next-mdx-remote"],
   images: {
     remotePatterns: [
